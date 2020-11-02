3c673a75b98f0ab567df978c4f3e983f1aac7370
Carl-Rabbit <479843162@qq.com>
2020-11-02 21:32:59 +0800

    Update database .sql

--- database/modeling/role/roles.sql
+++ database/modeling/role/roles.sql
@@ -8,3 +8,25 @@ create table roles
         constraint roles_username_1ukey unique,
     password  varchar not null
 );
+
+alter table roles add column
+    status char not null default 'V';       -- V: valid, D: deprecated
+
+alter table roles add constraint
+    roles_username_ukey unique (username);
+
+alter sequence roles_role_id_seq restart;
+
+truncate roles cascade;
+
+delete from roles where username = 'admin1';
+insert into roles (role_type, username, password)
+values ('A', 'admin1', 'admin1');
+
+delete from roles where username = 'testchertest1';
+insert into roles (role_type, username, password)
+values ('T', 'teachertest1', 'teachertest1');
+
+delete from roles where username = 'studenttest1';
+insert into roles (role_type, username, password)
+values ('S', 'studenttest1', 'studenttest1');
