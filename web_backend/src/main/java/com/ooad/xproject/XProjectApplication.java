ca97e6615c821ba3d7d8e51eb77b2a1b58626615
Carl-Rabbit <479843162@qq.com>
2020-11-06 12:08:54 +0800

    Add lombok and springfox dependencies

--- web_backend/src/main/java/com/ooad/xproject/XProjectApplication.java
+++ web_backend/src/main/java/com/ooad/xproject/XProjectApplication.java
@@ -3,7 +3,9 @@ package com.ooad.xproject;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableOpenApi
 @SpringBootApplication
 @MapperScan("com.ooad.xproject.mapper")
 public class XProjectApplication {
