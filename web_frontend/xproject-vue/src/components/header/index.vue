f99decf6f8e814643e936d30f97c1977c8541c1a
uni-horihori <71215499+uni-horihori@users.noreply.github.com>
2020-11-13 23:58:53 +0800

    update sidebar&header

--- web_frontend/xproject-vue/src/components/header/index.vue
+++ web_frontend/xproject-vue/src/components/header/index.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class = "header">
+  <div id = "header">
     <el-header>
-      <i class="el-icon-d-arrow-left" style="text-align: left"></i>
-      <i style="margin-right: 40px" class="el-icon-help"></i>
-      <i class ="el-icon-user"></i>
-
+      <i v-bind:class="[{'el-icon-d-arrow-right':isFolded},'el-icon-d-arrow-left']"></i>
+      <i style="margin-left: 90%" class="el-icon-help"></i>
+      <i style="margin-left: 30px" class ="el-icon-user"></i>
     </el-header>
   </div>
 </template>
 
 <script>
 export default {
-  name: "index"
+  name: "index",
+
+  data() {
+    return {
+      isFolded:false
+    }
+  }
 }
 </script>
 
 <style>
-.header {
+#header {
   background-color: #edf4ff;
   color: #333;
   line-height: 60px;
   width:100%;
   font-size: 20px;
-  text-align: right;
-
+  text-align: left;
 }
+
 </style>
--- web_frontend/xproject-vue/src/components/sidebar/index.vue
+++ web_frontend/xproject-vue/src/components/sidebar/index.vue
@@ -50,17 +50,17 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       isCollapse: false
-    };
+    }
   },
   methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
     },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
