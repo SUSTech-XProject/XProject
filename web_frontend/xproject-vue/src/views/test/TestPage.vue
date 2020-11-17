94b32e23c85095581a138b43af06ddec037eeb22
CharlotteE67 <71111432+CharlotteE67@users.noreply.github.com>
2020-11-17 13:03:41 +0800

    Sidebar update but not finished

--- web_frontend/xproject-vue/src/views/test/TestPage.vue
+++ web_frontend/xproject-vue/src/views/test/TestPage.vue
@@ -1,12 +1,13 @@
 <template>
-  <div id = "temp">
+  <div id = "Testpage">
     <el-container>
-      <el-aside style="width: 201px">
+      <el-aside style="width: 201px;height: 100%">
         <left-bar></left-bar>
       </el-aside>
         <el-container>
           <el-header>
-            <Header></Header>
+            <Header>
+            </Header>
           </el-header>
         <el-main>
           <DownDrop></DownDrop>
@@ -22,19 +23,46 @@ import LeftBar from '@/components/sidebar/index'
 import Header from '@/components/header/index'
 import Table from '@/components/table/index'
 import DownDrop from '@/components/downdrop/index'
+import Breadcrumb from '@/components/breadcrumb/index'
+
 export default{
   name: 'Testpage',
+  data () {
+    return {
+      // defaultHeight: {
+      //   height: ''
+      // }
+    }
+  },
   components: {
-    LeftBar, Header, Table, DownDrop
+    LeftBar, Header, Table, DownDrop, Breadcrumb
   }
+  // methods: {
+  //   getHeight () {
+  //     this.defaultHeight.height = window.innerHeight - 60 + 'px'
+  //   }
+  // },
+  // getHeight () {
+  //   this.defaultHeight.height = window.innerHeight - 60 + 'px'
+  // }
 }
 </script>
 
 <style>
+#Testpage{
+  height: 100%;
+}
 .header{
   width : 100%;
   float:right;
   height: 60px;
+}
+.el-aside{
+  position: relative;
+  padding: 0px;
+  margin: 0px;
+  height: 100%;
+  /*background: #42b983;*/
 }
 .el-header {
   background-color: #edf4ff;
@@ -51,7 +79,11 @@ export default{
 /*  text-align: center;*/
 /*  !*line-height: 200px;*!*/
 /*}*/
-
+.el-container{
+  padding: 0px;
+  margin: 0px;
+  height: 100%;
+}
 .el-main {
   background-color: #E9EEF3;
   color: #333;
