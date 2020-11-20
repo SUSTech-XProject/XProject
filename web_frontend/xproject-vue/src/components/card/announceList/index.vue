f0e62e4c51b2614c95f76f6fe3f7f26a453d5001
uni-horihori <71215499+uni-horihori@users.noreply.github.com>
2020-11-20 11:15:04 +0800

    complete function of drawer

--- web_frontend/xproject-vue/src/components/card/announceList/index.vue
+++ web_frontend/xproject-vue/src/components/card/announceList/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <el-card class="proj-card" shadow="hover">
-    <div class="clearfix" >
+  <el-card  class="proj-card" shadow="hover">
+    <div class="clearfix"  >
       <span><slot>{{annoTitle}}</slot></span>
       <el-button :icon = "iconData"
                  style="color: orange;float: right" type="text"
-                 @click="favour(annoID)"  circle></el-button>
+                 @click.stop="favour(annoID)"  circle></el-button>
     </div>
     <!--    <div v-for="o in 2" :key="o" class="text item">-->
     <!--      {{'列表内容 ' + o }}-->
@@ -16,7 +16,6 @@
 <script>
 
 export default {
-
   data() {
     return {
       annoID:this.id,
@@ -28,26 +27,11 @@ export default {
     }
   },
   props: {
-    id:{
-      type:Number,
-      default:-1
-    },
-    title:{
-      type:String,
-      default:'Announce 1'
-    },
-    author:{
-      type:String,
-      default: 'xxx'
-    },
-    date:{
-      type:String,
-      default:'2020-01-01'
-    },
-    star:{
-      type:Boolean,
-      default:false
-    }
+    id:{type:Number, default:-1},
+    title:{type:String, default:'Announce 1'},
+    author:{type:String, default: 'xxx'},
+    date:{type:String, default:'2020-01-01'},
+    star:{type:Boolean, default:false}
   },
 
   methods:{
@@ -55,7 +39,8 @@ export default {
       this.isStar= !this.isStar;
       this.iconData = this.isStar===true?'el-icon-star-on':'el-icon-star-off';
       this.$emit('getStarChange',val)
-    }
+    },
+
   }
 }
 </script>
---
+++ web_frontend/xproject-vue/src/components/drawer/announcement/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+<!--  <el-button @click="drawer = true" type="primary" style="margin-left: 16px;">-->
+<!--    点我打开-->
+<!--  </el-button>-->
+
+  <el-drawer
+    title="我是标题"
+    direction="rtl"
+    :with-header = "false"
+    :visible.sync="annoDrawer"
+    :before-close="handleClose">
+    <h2>{{annoTitle}}</h2>
+    <hr/>
+    <h4>Author: {{annoAuthor}}</h4>
+  </el-drawer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "index",
+  data() {
+    return {
+      annoDrawer: this.drawer,
+      annoTitle:this.title,
+      annoAuthor:this.author,
+      annoTime:this.time,
+      annoMessage:this.message,
+
+    };
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          this.refresh()
+          done();
+        })
+        .catch(_ => {});
+    },
+    refresh(){
+      console.log("close")
+      this.$emit('closeDrawer',"msg")
+    }
+  },
+  props:{
+    drawer:{type:Boolean,default:false},
+    title:{type:String, default:"title"},
+    author:{type:String,default: "admin"},
+    time:{type:String,default:"2020-01-01 9:34"},
+    message:{type:String,default:"message content"},
+  }
+};
+</script>
+
+<style>
+
+</style>
