39b61a6c2dbf4b420a4b77114cf4c06a2fa1c5d0
ArslanaWu <50170078+ArslanaWu@users.noreply.github.com>
2020-11-22 20:53:54 +0800

    restore MyTeam.vue

--- web_frontend/xproject-vue/src/views/main/project/team/MyTeam.vue
+++ web_frontend/xproject-vue/src/views/main/project/team/MyTeam.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <el-row style="margin-top: 20px">
-      <el-col :span="6" :offset="1">
-        <div>
+      <el-col :span="4" :offset="2">
+        <div align="left">
           <!--                <el-avatar :size="200"><div style="font-size: 80px; text-align:center; margin-top: -5px">n</div></el-avatar>-->
-          <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" :size="200"></el-avatar>
+          <el-avatar src="http://wx1.sinaimg.cn/large/0072GgNply1g5takxmar5j309q09w3yl.jpg"
+                     :size="150"></el-avatar>
         </div>
       </el-col>
 
-      <el-col :span="16" :offset="1">
-        <div style="font-size: 80px; padding: 0; text-align:left; margin-top: 20px">name</div>
+      <el-col :span="16">
+        <div style="font-size: 50px; padding:0; text-align:left; margin-top: 20px">Team</div>
         <div style="padding: 0; text-align:left; margin-top: 20px">
           <el-tag>标签一</el-tag>
           <el-tag type="success">标签二</el-tag>
@@ -19,17 +20,72 @@
         </div>
       </el-col>
     </el-row>
+
+    <div style="margin-top: 40px; margin-left:40px">
+      <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+        <el-tab-pane label="Introduction" name="intro">
+          <div style="font-size: 15px; padding:0; text-align:left; margin-top: 15px;margin-left: 20px">
+            Team for project {}
+          </div>
+          <div style="margin-top: 20px; margin-left: 20px">
+            <el-input
+              type="textarea"
+              :rows="5"
+              placeholder=""
+              v-model="textarea">
+            </el-input>
+          </div>
+        </el-tab-pane>
+
+        <el-tab-pane label="Member" name="member">
+          <el-row>
+            <el-col :span="2" class="left">
+              <el-avatar src="https://ww1.sinaimg.cn/thumb150/006z25tvly1genhm1qn9vj30tc0t9wi8.jpg"
+                         :size="50"></el-avatar>
+            </el-col>
+            <el-col :span="12">
+              <div style="font-size: 20px; padding:0; text-align:left; margin-top: 22px">name</div>
+            </el-col>
+          </el-row>
+
+          <el-row style="margin-top: 10px" class="left">
+            <el-col :span="2">
+              <el-avatar src="https://ww4.sinaimg.cn/orj360/0064eUp2ly1ge61i3malnj3073077746.jpg"
+                         :size="50"></el-avatar>
+            </el-col>
+            <el-col :span="12">
+              <div style="font-size: 20px; padding:0; text-align:left; margin-top: 15px">name</div>
+            </el-col>
+          </el-row>
+        </el-tab-pane>
+
+        <el-tab-pane label="Message" name="message">
+          message
+        </el-tab-pane>
+      </el-tabs>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name: "MyTeam",
-  components: {
+  name: "MyTeam",
+  components: {},
+  data() {
+    return {
+      activeName: 'intro'
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.left {
+  padding: 10px 0;
+}
 </style>
