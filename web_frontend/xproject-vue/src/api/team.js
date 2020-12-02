bf42d579f0bd54b664505ba5333ff585f3c40266
uni-horihori <71215499+uni-horihori@users.noreply.github.com>
2020-12-02 21:48:33 +0800

    update api requirements

--- web_frontend/xproject-vue/src/api/team.js
+++ web_frontend/xproject-vue/src/api/team.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
-export function getTeamInfo () {
+export function getTeamInfo (projId) {
   return Vue.prototype.$axios.get('/team-info')
+}
+export function getTeamDetail(teamId){
+  return Vue.prototype.$axios.get('/team-detail')
+}
+export function postApply(teamId){
+  return Vue.prototype.$axios.post('/team-apply', {
+    teamId:teamId,
+  })
+
 }
