33129dff01b159e2b000c726e313c847fb95c58d
Carl-Rabbit <479843162@qq.com>
2020-12-04 00:28:07 +0800

    Finished side bar bug fix

--- web_frontend/xproject-vue/src/components/sidebar/index.vue
+++ web_frontend/xproject-vue/src/components/sidebar/index.vue
@@ -1,28 +1,30 @@
 <template>
   <div style="height: 100%">
     <el-scrollbar>
-      <el-menu :default-active="$route.path"
+      <el-menu :default-active="activeIdx"
                class="el-menu-vertical-demo"
                id="left-bar"
                @open="handleOpen" @close="handleClose"
                @select="handleSelect"
                background-color="#304156"
                text-color="#bfcbd9"
-               active-text-color="#f4f4f5"
+               active-text-color="#4392f9"
                :collapse="isCollapse" style="height: 100%">
         <el-menu-item id="title" index="Welcome">
-          <i class="el-icon-first-aid-kit" ></i>
-          <span slot="title">XProject</span>
+          <i style="margin-top: -10px">
+            <img :src="logo" alt="logo" style="width: 35px; margin-left: -5px; padding-bottom: 6px"/>
+          </i>
+          <span slot="default" style="margin-left: 5px; font-size: 22px">XProject</span>
         </el-menu-item>
 
         <el-menu-item index="HomePage">
           <i class="el-icon-s-home"></i>
-          <span slot="default">Home Page</span>
+          <span slot="title">Home Page</span>
         </el-menu-item>
 
         <el-menu-item index="ProjectList">
           <i class="el-icon-notebook-1"></i>
-          <span slot="default">Project List</span>
+          <span slot="title">Project List</span>
         </el-menu-item>
 
         <el-menu-item index="Account">
@@ -30,11 +32,11 @@
           <span slot="title">Account</span>
         </el-menu-item>
 
-        <el-menu-item-group v-if="this.$store.state.proj.projId != null" id="proj-items">
-          <template slot="title"></template>
-
-          <el-menu-item index="ProjOverview" >
-            <i class="el-icon-date"></i>
+<!--        <el-menu-item-group v-if="this.$store.state.proj.projId != null" id="proj-items">-->
+<!--        </el-menu-item-group>-->
+        <div  v-if="this.$store.state.proj.projId != null" style="padding-top: 10px">
+          <el-menu-item index="ProjOverview">
+            <i class="el-icon-s-promotion"></i>
             <span slot="title">Overview</span>
           </el-menu-item>
 
@@ -45,12 +47,14 @@
               <span v-if="isCollapse" slot="title"></span>
             </template>
 
-            <el-menu-item-group>
-              <el-menu-item index="MyTeam">My Team</el-menu-item>
-              <el-menu-item index="Forming">Forming</el-menu-item>
-            </el-menu-item-group>
+            <el-menu-item index="MyTeam">My Team</el-menu-item>
+            <el-menu-item index="Forming">Forming</el-menu-item>
           </el-submenu>
 
+          <el-menu-item index="@3.5" >
+            <i class="el-icon-bell"></i>
+            <span slot="title">Announcements</span>
+          </el-menu-item>
           <el-menu-item index="@4" >
             <i class="el-icon-date"></i>
             <span slot="title">Events</span>
@@ -67,7 +71,9 @@
             <i class="el-icon-collection"></i>
             <span slot="title">Gradebook</span>
           </el-menu-item>
-        </el-menu-item-group>
+          <div style="height: 60px"></div>
+        </div>
+
 
       </el-menu>
       <!--    <el-radio-button :label="false">Open</el-radio-button>-->
@@ -76,22 +82,24 @@
       <!--      <el-radio-button :label="false">展开</el-radio-button>-->
       <!--      <el-radio-button :label="true">收起</el-radio-button>-->
 
+
       <el-radio-group v-model="isCollapse">
-        <el-button circle v-show="!isCollapse" class="el-icon-s-fold expandBtn" @click="isCollapse=true"></el-button>
-        <el-button circle v-show="isCollapse" class="el-icon-s-fold shrinkBtn" @click="isCollapse=false"></el-button>
+        <el-button circle v-show="!isCollapse" class="el-icon-s-fold" @click="isCollapse=true"></el-button>
+        <el-button circle v-show="isCollapse" class="el-icon-s-unfold" @click="isCollapse=false"></el-button>
       </el-radio-group>
     </el-scrollbar>
   </div>
 </template>
 
 <script>
-  import logo from '@/assets/xproject-icon.png'
+  import logo from '@/assets/xproject-icon-v3-1.png'
   export default {
     data () {
       return {
         isCollapse: false,
         show: false,
-        logo
+        logo,
+        activeIdx: '',
       }
     },
     methods: {
@@ -103,6 +111,7 @@
       },
       handleSelect (key, keyPath) {
         console.log(key, keyPath)
+        this.activeIdx = key
         if (key.indexOf("@") !== -1) {
           console.log('Not finished')
         } else {
@@ -123,35 +132,17 @@
 }
 
 .el-radio-group {
-  /*position: center;*/
   position: absolute;
+  right: 12px;
   bottom: 4%;
-  font-size: 16px;
-  /*line-height: 300%;*/
   line-height: 100%;
-  /*margin-bottom: 10px;*/
-  /*margin-top: 10%;*/
-  /*margin-bottom: 10%;*/
-  /*margin-top: 20px;*/
-  /*margin-left: 0px;*/
-  /*width: 200px;*/
-}
-.shrinkBtn {
-  position: center;
-  /*left: -18px;*/
-  /*top: 38px;*/
   color: #151d41;
-  /*margin-bottom: 10%;*/
-  transform: rotate(180deg);
 }
-.expandBtn {
-  /*position: center;*/
-  /*left: -18px;*/
-  /*top: 38px;*/
-  color: #151d41;
-  margin-left: 140px;
-  /*transform: rotate(0deg);*/
+
+.el-radio-group > .el-button {
+  font-size: 16px;
 }
+
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   min-height: 100%;
