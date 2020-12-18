a64a376561ebcf14cadba0f069008acb06188f9b
ArslanaWu <50170078+ArslanaWu@users.noreply.github.com>
2020-12-18 15:10:03 +0800

    part of scoring

--- web_frontend/xproject-vue/src/views/main/project/team/TeamManaging.vue
+++ web_frontend/xproject-vue/src/views/main/project/team/TeamManaging.vue
@@ -4,22 +4,26 @@
       <span id="title-text">Team Management</span>
     </div>
     <div id="card-body">
-
       <div style="text-align: right;padding-right: 30px">
         <el-button type="primary" plain
-                   icon="el-icon-circle-plus-outline" @click="addTeam">Create</el-button>
+                   icon="el-icon-circle-plus-outline" @click="addTeam">Create
+        </el-button>
         <el-button type="danger" plain
-                   icon="el-icon-delete" @click="deleteTeam">Delete</el-button>
+                   icon="el-icon-delete" @click="deleteTeam">Delete
+        </el-button>
         <el-button type="success" plain
-                   icon="el-icon-circle-check" @click="confirmTeam">Confirm</el-button>
+                   icon="el-icon-circle-check" @click="confirmTeam">Confirm
+        </el-button>
         <el-button type="warning" plain
-                   icon="el-icon-edit" @click="manageTeam">Manage</el-button>
+                   icon="el-icon-edit" @click="manageTeam">Manage
+        </el-button>
       </div>
+
       <el-card style="margin: 15px 0;height: 100%">
         <el-table
           height="380"
           ref="multipleTable"
-          v-loading = "tableLoading"
+          v-loading="tableLoading"
           :data="tableData"
           empty-text="No Data Found"
           tooltip-effect="dark"
@@ -29,12 +33,12 @@
             type="selection"
             width="55">
           </el-table-column>
-<!--          <el-table-column-->
-<!--            prop = "index"-->
-<!--            label="Index"-->
-<!--            width="120"-->
-<!--            sortable>-->
-<!--          </el-table-column>-->
+          <!--          <el-table-column-->
+          <!--            prop = "index"-->
+          <!--            label="Index"-->
+          <!--            width="120"-->
+          <!--            sortable>-->
+          <!--          </el-table-column>-->
           <el-table-column prop="name" label="Team Name" sortable show-overflow-tooltip></el-table-column>
           <el-table-column
             prop="topic"
@@ -45,19 +49,19 @@
             :filter-method="filterHandler">
           </el-table-column>
           <el-table-column
-            prop ="curMem"
+            prop="curMem"
             label="Current"
             width="150px"
             sortable>
           </el-table-column>
           <el-table-column
-            prop ="targetMem"
+            prop="targetMem"
             label="Size"
             width="150px"
             sortable>
           </el-table-column>
           <el-table-column
-            prop = "status"
+            prop="status"
             label="Status"
             width="150px"
             :filters="teamStatus"
@@ -70,8 +74,17 @@
                 size="mini"
                 type="primary" plain
                 icon="el-icon-search"
-                @click="openDrawer(scope.row.index)">Detail</el-button>
+                @click="openDrawer(scope.row.index)">Detail
+              </el-button>
             </template>
+          </el-table-column>
+          <el-table-column>
+            <el-button
+              size="mini"
+              type="primary" plain
+              icon="el-icon-document-checked"
+              @click="Scoring(scope.row.index)">Scoring
+            </el-button>
           </el-table-column>
         </el-table>
         <footer>
@@ -89,9 +102,9 @@
 
     <drawer :drawer.sync="drawerCtrl"
             :id="drawerId"
-            @closeDrawer = "closeDrawer"></drawer>
+            @closeDrawer="closeDrawer"></drawer>
 
-    <create :visible.sync="createVisible" @closeForm = 'closeForm'></create>
+    <create :visible.sync="createVisible" @closeForm='closeForm'></create>
     <forming :visible.sync="formingVisible"
              :team-in="multipleSelection"
              @closeManaging="closeManaging"></forming>
@@ -107,25 +120,25 @@ import {postTeamConfirmation} from '@/api/team'
 import AutoForming from '@/views/main/project/team/AutoForming'
 
 export default {
-  name: "TeamManaging",
-  components:{
-    drawer:teamdrawer,
-    create:CreateTeam,
-    forming:AutoForming
+  name: 'TeamManaging',
+  components: {
+    drawer: teamdrawer,
+    create: CreateTeam,
+    forming: AutoForming
   },
-  data(){
+  data () {
     return {
-      drawerCtrl:false,
-      drawerId:1,
+      drawerCtrl: false,
+      drawerId: 1,
       tableData: [],
-      topicFilter:[],
+      topicFilter: [],
       teamStatus: [
         {text: 'Raw', value: 'Raw'},
         {text: 'Confirm', value: 'Confirm'},
       ],
-      tableLoading:false,
-      createVisible:false,
-      formingVisible:false,
+      tableLoading: false,
+      createVisible: false,
+      formingVisible: false,
       multipleSelection: []
     }
   },
@@ -133,7 +146,7 @@ export default {
     this.reLoad()
   },
   methods: {
-    initTeams(){
+    initTeams () {
       let id = this.$store.state.proj.projId
       //console.log(id)
       getTeamInfoList(parseInt(id)).then(resp => {
@@ -146,34 +159,34 @@ export default {
 
         let teamList = resp.data.data
         console.log(teamList)
-        this.tableData.splice(0,this.tableData.length)
-        this.topicFilter.splice(0,this.topicFilter.length)
+        this.tableData.splice(0, this.tableData.length)
+        this.topicFilter.splice(0, this.topicFilter.length)
         for (let i = 0; i < teamList.length; i++) {
           let team = teamList[i]
           this.tableData.push({
-            index:team.projInstId,
+            index: team.projInstId,
             name: team.teamName,
             topic: team.topic,
-            targetMem:team.targetMemNum,
-            status:team.status,
-            curMem:team.curMemNum
+            targetMem: team.targetMemNum,
+            status: team.status,
+            curMem: team.curMemNum
           })
 
           this.topicFilter.push({
-            text:team.topic,
-            value:team.topic
+            text: team.topic,
+            value: team.topic
           })
         }
         this.topicFilter = this.unique(this.topicFilter)
         console.log(this.topicFilter)
-      }).catch(failResp=>{
+      }).catch(failResp => {
         console.log('fail in getProjList. message=' + failResp.message)
       })
     },
-    unique(arr){
+    unique (arr) {
       let result = {}
       let finalResult = []
-      for (let i = 0; i <arr.length ; i++) {
+      for (let i = 0; i < arr.length; i++) {
         result[arr[i].value] = arr[i]
       }
       for (const item in result) {
@@ -182,69 +195,68 @@ export default {
       return finalResult
     },
 
-    openDrawer(val){
+    openDrawer (val) {
       //console.log(val)
       this.drawerId = val
       this.drawerCtrl = true
     },
-    closeDrawer(){
+    closeDrawer () {
       this.drawerCtrl = false
     },
-    filterHandler(value, row, column) {
-      const property = column['property'];
-      return row[property] === value;
+    filterHandler (value, row, column) {
+      const property = column['property']
+      return row[property] === value
     },
-    handleSelectionChange(val) {
-      this.multipleSelection = val;
+    handleSelectionChange (val) {
+      this.multipleSelection = val
     },
     teamStatusFMethod (value, row, column) {
-      return value === row.status;
+      return value === row.status
     },
 
-    reLoad(){
+    reLoad () {
       this.tableLoading = true
       this.initTeams()
-      setTimeout(()=>{
+      setTimeout(() => {
         this.tableLoading = false
-      },1000)
-
+      }, 1000)
 
     },
 
-    addTeam(){
+    addTeam () {
       console.log(this.createVisible)
       this.createVisible = true
     },
-    closeForm(val){
+    closeForm (val) {
       this.createVisible = false
-      if(val===true){
+      if (val === true) {
         //创建队伍重新挂载
         this.reLoad()
       }
     },
-    manageTeam(){
+    manageTeam () {
       console.log(this.multipleSelection)
       this.formingVisible = true
       //调用组队表单？
     },
-    closeManaging(val){
+    closeManaging (val) {
       this.formingVisible = false
-      if(val){
+      if (val) {
         this.reLoad()
       }
     },
 
-    deleteTeam(){
-      if(this.multipleSelection.length===0){
+    deleteTeam () {
+      if (this.multipleSelection.length === 0) {
         this.$message.error('No team selected yet')
-      }else {
+      } else {
         this.$confirm('Delete selected teams?', 'Warning', {
           confirmButtonText: 'Confirm',
           cancelButtonText: 'Cancel',
           type: 'warning'
         }).then(() => {
           var list = []
-          for (let i = 0; i <this.multipleSelection.length ; i++) {
+          for (let i = 0; i < this.multipleSelection.length; i++) {
             list.push(parseInt(this.multipleSelection[i].index))
           }
           console.log(list)
@@ -255,7 +267,7 @@ export default {
               this.$message({
                 type: 'success',
                 message: 'Delete ' + num + ' teams successfully'
-              });
+              })
               this.reLoad()
             } else {
               this.$message.error(resp.data.message)
@@ -267,22 +279,22 @@ export default {
           this.$message({
             type: 'info',
             message: 'Canceled'
-          });
-        });
+          })
+        })
       }
     },
-    confirmTeam(){
+    confirmTeam () {
       console.log(this.multipleSelection)
-      if(this.multipleSelection.length===0){
+      if (this.multipleSelection.length === 0) {
         this.$message.error('No team selected yet')
-      }else {
+      } else {
         this.$confirm('Confirm selected teams?', 'Alert', {
           confirmButtonText: 'Confirm',
           cancelButtonText: 'Cancel',
           type: 'info'
         }).then(() => {
           const list = []
-          for (let i = 0; i <this.multipleSelection.length ; i++) {
+          for (let i = 0; i < this.multipleSelection.length; i++) {
             list.push(parseInt(this.multipleSelection[i].index))
           }
           console.log(list)
@@ -294,7 +306,7 @@ export default {
               this.$message({
                 type: 'success',
                 message: 'Confirm ' + num + ' teams successfully'
-              });
+              })
             } else {
               this.$message.error(resp.data.message)
             }
@@ -305,24 +317,28 @@ export default {
           this.$message({
             type: 'info',
             message: 'Canceled'
-          });
-        });
+          })
+        })
       }
     },
 
+    Scoring () {
 
+    }
   }
 }
 </script>
 
 <style scoped>
-#base-card{
+#base-card {
   margin: 15px 10px;
 }
+
 #title-text {
   font-size: 20px;
 }
-#card-body{
+
+#card-body {
 
 }
 </style>
