0856251a4e6ae8df2d1b2c381f06a4c3c5196846
uni-horihori <71215499+uni-horihori@users.noreply.github.com>
2020-12-21 22:36:33 +0800

    update projList page

--- web_frontend/xproject-vue/src/api/home_page.js
+++ web_frontend/xproject-vue/src/api/home_page.js
@@ -12,13 +12,16 @@ export function getProjListBySch () {
   return Vue.prototype.$axios.get('/all/project/by-sch')
 }
 
-export function getJoinProj (projId) {
-  return Vue.prototype.$axios.get('/student/project/join', {
-    params: {projId: projId}
+export function postJoinProj (projId,groupMark) {
+  return Vue.prototype.$axios.post('/all/project/join',null, {
+    params: {
+      projId: projId,
+      groupMark: groupMark
+    }
   })
 }
 export function postProjQuit(projId){
-  return Vue.prototype.$axios.post('/student/proj/quit',null,{
+  return Vue.prototype.$axios.post('/all/proj/quit',null,{
     params:{projId:parseInt(projId)}
   })
 }
